e2d0bec59843ac56189d5fc48f13981dc7d578e5
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-21 13:13:26 +0530

    Initial Commit

---
+++ myLib123/myLib123.h
@@ -0,0 +1,19 @@
+//
+//  myLib123.h
+//  myLib123
+//
+//  Created by Mac on 21/12/18.
+//  Copyright © 2018 tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for myLib123.
+FOUNDATION_EXPORT double myLib123VersionNumber;
+
+//! Project version string for myLib123.
+FOUNDATION_EXPORT const unsigned char myLib123VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <myLib123/PublicHeader.h>
+
+
