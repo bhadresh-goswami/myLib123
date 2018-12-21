6c7668bf9fbb7e0d18cb1cce53f29220545db842
tops-bhadresh <tops.bhadresh@gmail.com>
2018-12-21 13:15:44 +0530

    first commit

---
+++ myLib123/myLibTest.swift
@@ -0,0 +1,20 @@
+//
+//  myLibTest.swift
+//  myLib123
+//
+//  Created by Mac on 21/12/18.
+//  Copyright © 2018 tops. All rights reserved.
+//
+
+import Foundation
+
+class myLibTest{
+    
+    init() {
+        print("testing")
+    }
+    
+    public static func DoSomething(){
+        print("Process...")
+    }
+}
